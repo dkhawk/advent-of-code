275c0a1fd921a297fc8079d4fdbce870603d4d9e
Dale Hawkins <dkhawk@gmail.com>
2021-12-07 08:51:31 -0700

    Day 07 clean up

--- 2021/src/main/kotlin/day07/Day07.kt
+++ 2021/src/main/kotlin/day07/Day07.kt
@@ -1,15 +1,14 @@
 package day07
 
 import kotlin.math.abs
-import kotlin.math.pow
 import utils.Input
 
 @OptIn(ExperimentalStdlibApi::class)
 class Day07 {
   companion object {
     fun run() {
-//      Day07().part1()
-        Day07().part2()
+      Day07().part1()
+      Day07().part2()
     }
   }
 
@@ -25,43 +24,45 @@ class Day07 {
   }
 
   private fun part1() {
-    val inputs = getInput(useRealInput = true)
+    val crabs = getInput(useRealInput = true)
 
-    val crabs = inputs
+    val max = crabs.maxOf { it }
+    val min = crabs.minOf { it }
 
-    val m = crabs.maxOf { it }
-
-    val best = (0..m).map { position ->
-      position to crabs.map { abs(it - position) }.sum()
-    }.minByOrNull { it.second }
+    val best = minCost(min, max, crabs) { start, end ->
+      abs(end - start).toLong()
+    }
 
     println(best)
   }
 
   private fun part2() {
-    val inputs = getInput(useRealInput = true)
+    val crabs = getInput(useRealInput = true)
 
-    val crabs = inputs
+    val max = crabs.maxOf { it }
+    val min = crabs.minOf { it }
 
-    val m = crabs.maxOf { it }
+    val costValues = LongArray(max - min + 1)
+    costValues[0] = 0
+    costValues.indices.drop(1).forEach {
+      costValues[it] = costValues[it - 1] + it
+    }
 
-    val best = (0..m).map { position ->
-      position to crabs.map { cost(it, position) }.sum()
-    }.minByOrNull { it.second }
+    val best = minCost(min, max, crabs) { start, end ->
+      costValues[abs(end - start)]
+    }
 
     println(best)
   }
 
-  val costFunction = HashMap<Int, Long>()
-
-  private fun cost(it: Int, position: Int) : Long {
-    val d = abs(it - position)
-    return if (costFunction.containsKey(d)) {
-      costFunction[d]!!
-    } else {
-      val c = generateSequence(1L) { it + 1}.take(d).sum()
-      costFunction[d] = c
-      c
-    }
+  private fun minCost(
+    min: Int,
+    max: Int,
+    crabs: List<Int>,
+    costFunction: (Int, Int) -> Long
+  ): Pair<Int, Long>? {
+    return (min..max).map { position ->
+      position to crabs.sumOf { costFunction(it, position) }
+    }.minByOrNull { it.second }
   }
 }
